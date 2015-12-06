c1b98a58e40c4121bf041317d805279f6c7111dc
xCristinaS <cristina.sola.1992@gmail.com>
2015-12-06 10:08:51 +0100

    empezando

---
+++ Trabajo01/app/src/main/java/cristinasola/trabajo01/SecundaryActivity.java
@@ -0,0 +1,13 @@
+package cristinasola.trabajo01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecundaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secundary);
+    }
+}
