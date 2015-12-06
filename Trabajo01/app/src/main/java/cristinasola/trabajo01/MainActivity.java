7cdd64a113f4836d81b68b38f2174b33cd4884e4
xCristinaS <cristina.sola.1992@gmail.com>
2015-12-06 13:44:05 +0100

    avanzando un poquitin mas

--- Trabajo01/app/src/main/java/cristinasola/trabajo01/MainActivity.java
+++ Trabajo01/app/src/main/java/cristinasola/trabajo01/MainActivity.java
@@ -3,6 +3,9 @@ package cristinasola.trabajo01;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +27,30 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaccion = gestor.beginTransaction();
         transaccion.replace(id, FragmentoPrincipal.newInstance());
         transaccion.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        boolean r;
+        switch (item.getItemId()){
+            case R.id.agregar:
+
+                r = true;
+                break;
+            default:
+                r = super.onOptionsItemSelected(item);
+        }
+        return r;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
     }
 }
