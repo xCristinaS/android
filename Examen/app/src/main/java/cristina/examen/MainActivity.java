b565d8bbb4e110e6643aac24c0d6879ff7757227
xCristinaS <cristina.sola.1992@gmail.com>
2015-12-11 11:02:47 +0100

    avanzando

--- Examen/app/src/main/java/cristina/examen/MainActivity.java
+++ Examen/app/src/main/java/cristina/examen/MainActivity.java
@@ -3,6 +3,7 @@ package cristina.examen;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.util.SparseBooleanArray;
 import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +12,8 @@ import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
                 cargarSinopsisLibroSeleccionado(position);
             }
         });
-/*
+
         lstLibros.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
         lstLibros.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
             @Override
@@ -61,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-                mode.getMenuInflater().inflate(R.menu.menu_lista_alumnos, menu);
+                mode.getMenuInflater().inflate(R.menu.menu_context_borrar, menu);
                 return true;
             }
 
@@ -69,21 +72,22 @@ public class MainActivity extends AppCompatActivity {
             public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.borrar:
-                        ArrayList<Alumno> elems = getElementosSeleccionados(lstLibros, true);
+                        ArrayList<Libro> elems = getElementosSeleccionados(lstLibros, true);
                         // Se eliminan del adaptador.
-                        for (Alumno elemento : elems) {
-                            ((PersonalAdapterLista) lstLibros.getAdapter()).getAlumnos().remove(elemento);
+                        for (Libro elemento : elems) {
+                            ((PersonalAdapterLista) lstLibros.getAdapter()).getLibros().remove(elemento);
                         }
                         // Se notifica al adaptador que ha habido cambios.
                         ((PersonalAdapterLista) lstLibros.getAdapter()).notifyDataSetChanged();
-                        if (BddAlumnos.getAlumnos().size() == 0)
-                            listener.listaAlumnosVacia();
+
+                        //if (BddAlumnos.getAlumnos().size() == 0)
+                          //  listener.listaAlumnosVacia();
                         break;
                 }
                 return false;
             }
         });
-        */
+
     }
 
     private void cargarSinopsisLibroSeleccionado(int position){
@@ -91,5 +95,27 @@ public class MainActivity extends AppCompatActivity {
         lblSinopsis.setText(libroSeleccionado.getSinopsis());
         lblTituloSinopsis.setText(libroSeleccionado.getTitulo());
         cvSinopsis.setVisibility(View.VISIBLE);
+    }
+
+    private ArrayList<Libro> getElementosSeleccionados(ListView lista, boolean desmarcar){
+        ArrayList<Libro> datos = new ArrayList<>();
+        if (datos.size() > 0)
+            datos.removeAll(datos);
+        // Se obtienen los elementos seleccionados de la lista.
+        SparseBooleanArray selec = lista.getCheckedItemPositions();
+        for (int i = 0; i < selec.size(); i++) {
+            // Si está seleccionado.
+            if (selec.valueAt(i)) {
+                int position = selec.keyAt(i);
+                // Se quita de la selección (si hay que hacerlo).
+                if (desmarcar) {
+                    lista.setItemChecked(position, false);
+                }
+                // Se añade al resultado.
+                datos.add((Libro) lista.getItemAtPosition(selec.keyAt(i)));
+            }
+        }
+        // Se retorna el resultado.
+        return datos;
     }
 }
