6ac0be0ccefe206221586eb09c8362df202fff82
xCristinaS <cristina.sola.1992@gmail.com>
2015-12-11 10:51:57 +0100

    avanzando

--- Examen/app/src/main/java/cristina/examen/Coleccion.java
+++ Examen/app/src/main/java/cristina/examen/Coleccion.java
@@ -17,6 +17,37 @@ public class Coleccion {
                 "\n" +
                 "La inexperta e inocente Ana intenta olvidarle, pero pronto comprende cuánto le desea. Cuando la pareja por fin inicia una apasionada relación, Ana se sorprende por las peculiares prácticas eróticas de Grey, al tiempo que descubre los límites de sus propios y más oscuros deseos…\n";
         bdd.add(new Libro("50 sombras de Grey","E.L. James", "2011", sinopsis, "http://s.libertaddigital.com/fotos/noticias/cincuentasombras.jpg"));
+
+
+
+
+
+        sinopsis = "GANAR SIGNIFICA FAMA Y RIQUEZA. PERDER SIGNIFICA UNA MUERTE SEGURA. En una oscura versión del futuro próximo, doce chicos y doce chicas se ven obligados a participar en un reality show llamado Los juegos del hambre. Solo hay una regla: matar o morir. Cuando Katniss Everdeen, una joven de dieciséis años se presenta voluntaria para ocupar el lugar de su hermana en los juegos, lo entiende como una condena a muerte. Sin embargo Katniss ya ha visto la muerte de cerca y la supervivencia forma parte de su naturaleza. ¡Que empiecen los septuagésimo cuartos juegos del hambre!";
+        bdd.add(new Libro("Los juegos del hambre","Suzanne Collins", "2008", sinopsis, "http://www.blogjuegosdelhambre.com/wp-content/uploads/LJDH2.jpg"));
+        sinopsis = "El arma más poderosa creada por el hombre, una organización secreta sedienta de venganza... y apenas unas horas para evitar el desastre. La eterna pugna entre ciencia y religión se ha convertido en una guerra muy real. En un laboratorio de máxima seguridad, aparece asesinado un científico con un extraño símbolo grabado a fuego en su pecho. Para el profesor Robert Langdon no hay duda: los Illuminati, los hombres enfrentados a la Iglesia desde los tiempos de Galileo, han regresado. Y esta vez disponen de la más mortífera arma que ha creado la humanidad, un artefacto con el que pueden ganar la batalla final contra su eterno enemigo. Acompañado de una joven científica y un audaz capitán de la Guardia Suiza, Langdon comienza una carrera contra reloj, en una búsqueda desesperada por los rincones más secretos de El Vaticano.";
+        bdd.add(new Libro("Ángeles y demonios","Dan Brown", "2000", sinopsis, "http://s1.trrsf.com/blogs/331/files/image/angelesdemonios2.jpg"));
+        sinopsis = "Cuando la estudiante de Literatura Anastasia Steele recibe el encargo de entrevistar al exitoso y joven empresario Christian Grey, queda impresionada al encontrarse ante un hombre atractivo, seductor y también muy intimidante.\n" +
+                "\n" +
+                "La inexperta e inocente Ana intenta olvidarle, pero pronto comprende cuánto le desea. Cuando la pareja por fin inicia una apasionada relación, Ana se sorprende por las peculiares prácticas eróticas de Grey, al tiempo que descubre los límites de sus propios y más oscuros deseos…\n";
+        bdd.add(new Libro("50 sombras de Grey","E.L. James", "2011", sinopsis, "http://s.libertaddigital.com/fotos/noticias/cincuentasombras.jpg"));
+
+        sinopsis = "GANAR SIGNIFICA FAMA Y RIQUEZA. PERDER SIGNIFICA UNA MUERTE SEGURA. En una oscura versión del futuro próximo, doce chicos y doce chicas se ven obligados a participar en un reality show llamado Los juegos del hambre. Solo hay una regla: matar o morir. Cuando Katniss Everdeen, una joven de dieciséis años se presenta voluntaria para ocupar el lugar de su hermana en los juegos, lo entiende como una condena a muerte. Sin embargo Katniss ya ha visto la muerte de cerca y la supervivencia forma parte de su naturaleza. ¡Que empiecen los septuagésimo cuartos juegos del hambre!";
+        bdd.add(new Libro("Los juegos del hambre","Suzanne Collins", "2008", sinopsis, "http://www.blogjuegosdelhambre.com/wp-content/uploads/LJDH2.jpg"));
+        sinopsis = "El arma más poderosa creada por el hombre, una organización secreta sedienta de venganza... y apenas unas horas para evitar el desastre. La eterna pugna entre ciencia y religión se ha convertido en una guerra muy real. En un laboratorio de máxima seguridad, aparece asesinado un científico con un extraño símbolo grabado a fuego en su pecho. Para el profesor Robert Langdon no hay duda: los Illuminati, los hombres enfrentados a la Iglesia desde los tiempos de Galileo, han regresado. Y esta vez disponen de la más mortífera arma que ha creado la humanidad, un artefacto con el que pueden ganar la batalla final contra su eterno enemigo. Acompañado de una joven científica y un audaz capitán de la Guardia Suiza, Langdon comienza una carrera contra reloj, en una búsqueda desesperada por los rincones más secretos de El Vaticano.";
+        bdd.add(new Libro("Ángeles y demonios","Dan Brown", "2000", sinopsis, "http://s1.trrsf.com/blogs/331/files/image/angelesdemonios2.jpg"));
+        sinopsis = "Cuando la estudiante de Literatura Anastasia Steele recibe el encargo de entrevistar al exitoso y joven empresario Christian Grey, queda impresionada al encontrarse ante un hombre atractivo, seductor y también muy intimidante.\n" +
+                "\n" +
+                "La inexperta e inocente Ana intenta olvidarle, pero pronto comprende cuánto le desea. Cuando la pareja por fin inicia una apasionada relación, Ana se sorprende por las peculiares prácticas eróticas de Grey, al tiempo que descubre los límites de sus propios y más oscuros deseos…\n";
+        bdd.add(new Libro("50 sombras de Grey","E.L. James", "2011", sinopsis, "http://s.libertaddigital.com/fotos/noticias/cincuentasombras.jpg"));
+
+        sinopsis = "GANAR SIGNIFICA FAMA Y RIQUEZA. PERDER SIGNIFICA UNA MUERTE SEGURA. En una oscura versión del futuro próximo, doce chicos y doce chicas se ven obligados a participar en un reality show llamado Los juegos del hambre. Solo hay una regla: matar o morir. Cuando Katniss Everdeen, una joven de dieciséis años se presenta voluntaria para ocupar el lugar de su hermana en los juegos, lo entiende como una condena a muerte. Sin embargo Katniss ya ha visto la muerte de cerca y la supervivencia forma parte de su naturaleza. ¡Que empiecen los septuagésimo cuartos juegos del hambre!";
+        bdd.add(new Libro("Los juegos del hambre","Suzanne Collins", "2008", sinopsis, "http://www.blogjuegosdelhambre.com/wp-content/uploads/LJDH2.jpg"));
+        sinopsis = "El arma más poderosa creada por el hombre, una organización secreta sedienta de venganza... y apenas unas horas para evitar el desastre. La eterna pugna entre ciencia y religión se ha convertido en una guerra muy real. En un laboratorio de máxima seguridad, aparece asesinado un científico con un extraño símbolo grabado a fuego en su pecho. Para el profesor Robert Langdon no hay duda: los Illuminati, los hombres enfrentados a la Iglesia desde los tiempos de Galileo, han regresado. Y esta vez disponen de la más mortífera arma que ha creado la humanidad, un artefacto con el que pueden ganar la batalla final contra su eterno enemigo. Acompañado de una joven científica y un audaz capitán de la Guardia Suiza, Langdon comienza una carrera contra reloj, en una búsqueda desesperada por los rincones más secretos de El Vaticano.";
+        bdd.add(new Libro("Ángeles y demonios","Dan Brown", "2000", sinopsis, "http://s1.trrsf.com/blogs/331/files/image/angelesdemonios2.jpg"));
+        sinopsis = "Cuando la estudiante de Literatura Anastasia Steele recibe el encargo de entrevistar al exitoso y joven empresario Christian Grey, queda impresionada al encontrarse ante un hombre atractivo, seductor y también muy intimidante.\n" +
+                "\n" +
+                "La inexperta e inocente Ana intenta olvidarle, pero pronto comprende cuánto le desea. Cuando la pareja por fin inicia una apasionada relación, Ana se sorprende por las peculiares prácticas eróticas de Grey, al tiempo que descubre los límites de sus propios y más oscuros deseos…\n";
+        bdd.add(new Libro("50 sombras de Grey","E.L. James", "2011", sinopsis, "http://s.libertaddigital.com/fotos/noticias/cincuentasombras.jpg"));
     }
 
     public static void agregarLibro(Libro libro){
@@ -25,6 +56,10 @@ public class Coleccion {
 
     public static void eliminarLibro(Libro libro){
         bdd.remove(libro);
+    }
+
+    public static Libro getLibroAtIndex(int position){
+        return bdd.get(position);
     }
 
     public static ArrayList<Libro> getLibros(){
--- Examen/app/src/main/java/cristina/examen/MainActivity.java
+++ Examen/app/src/main/java/cristina/examen/MainActivity.java
@@ -2,12 +2,22 @@ package cristina.examen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView lstLibros;
     PersonalAdapterLista adaptador;
+    CardView cvSinopsis;
+    TextView lblSinopsis, lblTituloSinopsis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +28,68 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initViews(){
+        cvSinopsis = (CardView) findViewById(R.id.cvSinopsis);
+        lblSinopsis = (TextView) findViewById(R.id.lblSinopsis);
+        lblTituloSinopsis = (TextView) findViewById(R.id.lblTituloSinopsis);
         adaptador = new PersonalAdapterLista(this, Coleccion.getLibros());
         lstLibros = (ListView) findViewById(R.id.lstLibros);
         lstLibros.setAdapter(adaptador);
+
+        lstLibros.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                cargarSinopsisLibroSeleccionado(position);
+            }
+        });
+/*
+        lstLibros.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        lstLibros.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+                mode.setTitle(lstLibros.getCheckedItemCount() + getString(R.string.de) + lstLibros.getCount());
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                mode.getMenuInflater().inflate(R.menu.menu_lista_alumnos, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.borrar:
+                        ArrayList<Alumno> elems = getElementosSeleccionados(lstLibros, true);
+                        // Se eliminan del adaptador.
+                        for (Alumno elemento : elems) {
+                            ((PersonalAdapterLista) lstLibros.getAdapter()).getAlumnos().remove(elemento);
+                        }
+                        // Se notifica al adaptador que ha habido cambios.
+                        ((PersonalAdapterLista) lstLibros.getAdapter()).notifyDataSetChanged();
+                        if (BddAlumnos.getAlumnos().size() == 0)
+                            listener.listaAlumnosVacia();
+                        break;
+                }
+                return false;
+            }
+        });
+        */
+    }
+
+    private void cargarSinopsisLibroSeleccionado(int position){
+        Libro libroSeleccionado = Coleccion.getLibroAtIndex(position);
+        lblSinopsis.setText(libroSeleccionado.getSinopsis());
+        lblTituloSinopsis.setText(libroSeleccionado.getTitulo());
+        cvSinopsis.setVisibility(View.VISIBLE);
     }
 }
