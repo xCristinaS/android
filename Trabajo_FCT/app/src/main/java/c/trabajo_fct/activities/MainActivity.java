40cc1171ed71bfb29027ed17c77f8e08acfb2595
xCristinaS <cristina.sola.1992@gmail.com>
2016-02-27 15:59:12 +0100

    tabLayout, viewPager y navigationDrawer

--- Trabajo_FCT/app/src/main/java/c/trabajo_fct/activities/MainActivity.java
+++ Trabajo_FCT/app/src/main/java/c/trabajo_fct/activities/MainActivity.java
@@ -3,6 +3,10 @@ package c.trabajo_fct.activities;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,9 +17,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
 import c.trabajo_fct.R;
+import c.trabajo_fct.adapters.CachedFragmentPagerAdapter;
+import c.trabajo_fct.fragments.PaginaFragment;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private PaginasAdapter mAdaptador;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        initViews();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -39,6 +52,52 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void initViews() {
+        tabLayout = (TabLayout) findViewById(R.id.tabL);
+        setupViewPager();
+    }
+
+    private void setupViewPager() {
+        // Se crea y asigna el adaptador de páginas al viewPager.
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        ArrayList<String> titulosPaginas = new ArrayList<>();
+        titulosPaginas.add("Alumnos"); titulosPaginas.add("Empresas"); titulosPaginas.add("Visitas");
+        mAdaptador = new PaginasAdapter(getSupportFragmentManager(), titulosPaginas);
+        viewPager.setAdapter(mAdaptador);
+        viewPager.setOffscreenPageLimit(2);
+        // Se configura el tabLayout para que trabaje con el viewPager.
+        tabLayout.setupWithViewPager(viewPager);
+        // Se añade un listener para que al cambiar la página se cambia la
+        // imagen de cabecera.
+        //final ImageView imgCabecera = (ImageView) findViewById(R.id.imgCabecera);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            //final int[] imagenes = {R.drawable.foto0, R.drawable.foto1, R.drawable.foto2};
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                // Se establece la nueva imagen de cabecera y se fuerza el repintado.
+                //imgCabecera.setImageResource(imagenes[position]);
+                //imgCabecera.invalidate();
+                // Se muestra u oculta el FAB.
+/*                if (position == 1) {
+                    fab.hide();
+                } else {
+                    fab.show();
+                }*/
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+
+        });
     }
 
     @Override
@@ -89,5 +148,30 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    
+    public class PaginasAdapter extends CachedFragmentPagerAdapter {
+
+        private final ArrayList<String> mTitulos;
+
+        public PaginasAdapter(FragmentManager fm, ArrayList<String> titulos) {
+            super(fm);
+            this.mTitulos = titulos;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return PaginaFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            return mTitulos.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitulos.get(position);
+        }
     }
 }
---
+++ Trabajo_FCT/app/src/main/java/c/trabajo_fct/fragments/PaginaFragment.java
@@ -0,0 +1,67 @@
+package c.trabajo_fct.fragments;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import c.trabajo_fct.R;
+
+/**
+ * Created by Cristina on 27/02/2016.
+ */
+public class PaginaFragment extends Fragment {
+
+    private static final String ARG_NUMERO_PAGINA = "numero_pagina";
+    private static final String STATE_TEXTO = "state_texto";
+
+    private TextView lblTexto;
+
+    public PaginaFragment() {}
+
+    // Retorna el fragmento configurado. Recibe el número de página.
+    public static PaginaFragment newInstance(int numeroPagina) {
+        PaginaFragment fragment = new PaginaFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_NUMERO_PAGINA, numeroPagina);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.content_main, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // Se escribe el texto adecuado.
+        if (savedInstanceState != null) {
+            //mTexto = savedInstanceState.getString(STATE_TEXTO);
+        } else {
+            //mTexto = getString(R.string.numero_pagina, getArguments().getInt(ARG_NUMERO_PAGINA));
+        }
+        if (getView() != null) {
+            lblTexto = (TextView) getView().findViewById(R.id.prueba);
+        }
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Se almacena el texto mostrado.
+        //outState.putString(STATE_TEXTO, mTexto);
+    }
+
+    // Cuando se hace click sobre el fab estando en dicho fragmento.
+    public void fabOnClick(View view) {
+        //mTexto = getString(R.string.has_pulsado_sobre_el_fab, lblTexto.getText());
+        //lblTexto.setText(mTexto);
+        //Snackbar.make(view,mTexto, Snackbar.LENGTH_LONG).show();
+    }
+}
